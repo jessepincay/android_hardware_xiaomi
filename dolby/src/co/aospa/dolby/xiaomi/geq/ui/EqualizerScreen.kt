3e8ecd9096ead3742d2df6cf7ffef70330754008
klozz <carlosj@klozz.dev>
2024-10-09 13:35:40 +0000

    dolby:EqualizerScreen fix build on A15
    Signed-off-by: klozz <carlosj@klozz.dev>

--- dolby/src/co/aospa/dolby/xiaomi/geq/ui/EqualizerScreen.kt
+++ dolby/src/co/aospa/dolby/xiaomi/geq/ui/EqualizerScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.android.settingslib.spa.framework.theme.SettingsDimension
 import com.android.settingslib.spa.framework.theme.SettingsTheme
 
 @Composable
